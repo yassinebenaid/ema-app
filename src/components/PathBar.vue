6c326c87662f4352697fe20c9397824320adbbcb
yassinebenaid <yassinebenaide3@gmail.com>
2025-02-12 21:43:39 +0100

    added path tracker to home

---
+++ src/components/PathBar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{
+	segments: string[]
+}>()
+</script>
+
+<template>
+	<div class="text-xs flex items-center gap-1">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-4"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+			/>
+		</svg>
+
+		<template v-for="(segment, index) in segments" :key="index">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-3 text-gray-500"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+			</svg>
+			{{ segment }}
+		</template>
+	</div>
+</template>
--- src/views/Home/Main.vue
+++ src/views/Home/Main.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import PathBar from '@/components/PathBar.vue'
 import Sidebar from './Partials/Sidebar.vue'
 </script>
 
 <template>
 	<div class="w-full h-screen flex">
 		<Sidebar />
-		<main class="w-full flex">main</main>
+
+		<main class="w-full flex p-2">
+			<header>
+				<PathBar :segments="['Events']" />
+			</header>
+		</main>
 	</div>
 </template>
