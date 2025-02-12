9136b269d8d6ee0230a44cf6f7b152332d65e3d7
yassinebenaid <yassinebenaide3@gmail.com>
2025-02-12 12:34:19 +0100

    unify input component and display validation erorrs

---
+++ src/components/Input.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+	label: string
+	name: string
+	placeholder: string
+	required?: boolean
+	type?: 'text' | 'email' | 'password'
+	error?: string
+	autocomplete?: string
+}>()
+
+const id =
+	props.name?.toLowerCase().replace(/[^a-z]/, '') + Date.now().toString(36) + Math.random().toString(36)
+</script>
+
+<template>
+	<div class="w-full">
+		<label :for="id" class="block p-1 text-sm font-medium text-secondary">
+			{{ label }} <span v-if="required" class="text-orange-500">*</span>
+		</label>
+		<input
+			:id="id"
+			:placeholder="placeholder"
+			:type="type ?? 'text'"
+			:required="required"
+			:name="name"
+			:class="error && 'border-red-300 focus:ring-red-200'"
+			:autocomplete="autocomplete ?? 'none'"
+			class="w-full px-4 py-2 transition-all border outline-none rounded-lg placeholder:text-tertiary/60"
+		/>
+	</div>
+</template>
--- src/compose/http.ts
+++ src/compose/http.ts
@@ -1,5 +1,6 @@
 import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
+import { ref } from 'vue'
 
 const instance = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
@@ -8,6 +9,25 @@ const instance = axios.create({
 	},
 })
 
-export default function useHttp(config: AxiosRequestConfig) {
-	return instance(config)
+export default function useHttp() {
+	const loading = ref(false)
+	const errors = ref([])
+
+	const execute = (config: AxiosRequestConfig) => {
+		loading.value = true
+
+		instance(config)
+			.then(res => {
+				errors.value = res.data?.errors
+			})
+			.finally(() => {
+				loading.value = false
+			})
+	}
+
+	return {
+		loading,
+		errors,
+		execute,
+	}
 }
