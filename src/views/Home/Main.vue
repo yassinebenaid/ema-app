d25bb392c56ac3946310b70764be43e7a2242b41
yassinebenaid <yassinebenaide3@gmail.com>
2025-02-12 21:53:19 +0100

    make a separate global layout

--- src/views/Home/Main.vue
+++ src/views/Home/Main.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import PathBar from '@/components/PathBar.vue'
-import Sidebar from './Partials/Sidebar.vue'
+import Layout from '../Layout.vue'
 </script>
 
 <template>
-	<div class="w-full h-screen flex">
-		<Sidebar />
-
-		<main class="w-full flex p-2">
-			<header>
-				<PathBar :segments="['Events']" />
-			</header>
-		</main>
-	</div>
+	<Layout>
+		<h1 class="text-2xl font-bold">Events</h1>
+	</Layout>
 </template>
