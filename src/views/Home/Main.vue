6041a9295ecb220b41a2b6d755895aaebe7d7398
yassinebenaid <yassinebenaide3@gmail.com>
2025-02-13 07:35:30 +0100

    improve events UI

--- src/views/Home/Main.vue
+++ src/views/Home/Main.vue
@@ -67,6 +67,66 @@ import Layout from '../Layout.vue'
 
 					<span class="text-xs text-gray-500"> Technopark, Casablanca </span>
 				</div>
+				<div class="flex items-center gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-4 text-gray-400"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+						/>
+					</svg>
+
+					<div class="flex items-center -space-x-2 overflow-hidden">
+						<img
+							src="https://randomuser.me/api/portraits/women/79.jpg"
+							class="size-6 rounded-full border-2 border-white"
+						/>
+						<p class="text-xs text-gray-400 pl-3">Yassine Benaid</p>
+					</div>
+				</div>
+				<div class="flex items-center gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-4 text-gray-400"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+						/>
+					</svg>
+
+					<div class="flex items-center -space-x-2 overflow-hidden">
+						<img
+							src="https://randomuser.me/api/portraits/women/79.jpg"
+							class="size-6 rounded-full border-2 border-white"
+						/>
+						<img
+							src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg"
+							class="size-6 rounded-full border-2 border-white"
+						/>
+						<img
+							src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f"
+							class="size-6 rounded-full border-2 border-white"
+						/>
+						<img
+							src="https://randomuser.me/api/portraits/men/86.jpg"
+							class="size-6 rounded-full border-2 border-white"
+						/>
+						<p class="text-xs text-gray-400 pl-3">+5K attendees</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</Layout>
