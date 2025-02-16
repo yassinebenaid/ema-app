1194a0c13f34da58e43c5c9823939d724882b2ec
yassinebenaid <yassinebenaide3@gmail.com>
2025-02-16 12:50:57 +0100

    added tabs to event details

--- src/views/events/View.vue
+++ src/views/events/View.vue
@@ -11,6 +11,7 @@ const props = defineProps<{
 
 const event = ref<Event>()
 const { loading, execute } = useHttp()
+const tab = ref<'attendees' | 'comments'>('attendees')
 
 const load = () => {
 	execute({
@@ -31,7 +32,7 @@ onMounted(load)
 
 <template>
 	<Layout>
-		<div v-if="!loading" class="flex">
+		<div class="flex">
 			<div>
 				<div class="flex items-center justify-between">
 					<h1 class="text-2xl font-bold">{{ event?.title }}</h1>
@@ -184,6 +185,52 @@ onMounted(load)
 				</button>
 			</div>
 		</div>
-		<div v-else>loqding</div>
+		<div class="border-b flex items-center text-sm text-gray-500 font-medium">
+			<div
+				role="button"
+				@click="tab = 'attendees'"
+				:class="{ 'text-primary  border-primary': tab == 'attendees' }"
+				class="p-3 flex items-center border-b-2 border-transparent gap-2"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="size-5"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+					/>
+				</svg>
+				Attendees
+			</div>
+			<div
+				role="button"
+				@click="tab = 'comments'"
+				:class="{ 'text-primary  border-primary': tab == 'comments' }"
+				class="p-3 flex items-center border-b-2 border-transparent gap-2"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="size-5"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+					/>
+				</svg>
+
+				Comments
+			</div>
+		</div>
 	</Layout>
 </template>
